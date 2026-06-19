511a4ea8b3624ec2d1497442364e9cd40c70f2a0
maybenischal <nischaldhakal1409@gmail.com>
2026-06-20 02:02:39 +0545

    Add conversion of 1D to 2D vector

--- array.py
+++ array.py
@@ -17,3 +17,29 @@ print("________________________________")
 
 array5 = np.linspace(0,10, num=3) #start end and number of elements spaced linearly
 print(array5)
+
+#Reshape an array
+a = np.arange(6)
+print(a)
+
+b = a.reshape(3,2) #reshapes array into 3 row and 2 columns
+print(b)
+print("------------")
+
+#Converting 1D array to 2D Array
+c = np.arange(1,6)
+print(c)
+print(c.shape)
+print("------------")
+print("------------")
+
+c2 = a[np.newaxis,:] #converts to row vector
+print(c2)
+print(c2.shape) 
+
+print("------------")
+print("------------")
+
+c3 = a[:,np.newaxis] #converts into column vector
+print(c3)
+print(c3.shape) 

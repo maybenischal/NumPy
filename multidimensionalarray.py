79704b3e4d458c410d19743968eb0a8a68f51c13
maybenischal <nischaldhakal1409@gmail.com>
2026-06-19 21:49:38 +0545

    Add multidimensional array indexing

--- multidimensionalarray.py
+++ multidimensionalarray.py
@@ -6,5 +6,16 @@ print(array1.ndim)
 array2 = np.array([1, 2, 3, 4, 5])
 print(array2.ndim)  
 
-array3 = np.array([[1, 2, 3], [4, 5, 6]])
+array3 = np.array([[1, 2, 3],
+                   [4, 5, 6]])
 print(array3.ndim)
+
+array4 = np.array([
+                  [[1,2],[5,6]],
+                  [[3,4],[7,8]],
+                  [[13,14],[17,65]]
+                  ])
+print(array4.ndim) # calculates the dimension of the array
+print(array4.shape) # calculates the shappe - depth, row and column of the array
+ 
+print(array4[1,1,0]) # access the element at depth 1, row 1 and column 0 also known as mUltidimensional array indexing

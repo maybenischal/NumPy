e12f72f780c7f01769d9433a26197a6efc96322e
maybenischal <nischaldhakal1409@gmail.com>
2026-06-25 20:53:34 +0545

    add aggregate functions

---
+++ aggregatefunction.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array =  np.array([[1,2,3,4],
+                   [5,6,7,8]])
+
+print(np.sum(array))
+print(np.mean(array)) 
+print(np.std(array)) #standard deviation
+print(np.var(array)) #variance
+print(np.max(array))
+print(np.min(array))
+print(np.argmax(array)) #position of the max value in array
